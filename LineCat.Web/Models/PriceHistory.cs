74c946b65302e2bda751e46d2eaf7b516ed2fff0
lc1055 <lc1055@163.com>
2015-12-02 14:15:43 +0800

    init db

---
+++ LineCat.Web/Models/PriceHistory.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LineCat.Web.Models
+{
+    public class PriceHistory
+    {
+        public string ID { get; set; }
+        public string ProductID { get; set; }
+        public double Price { get; set; }
+        public int IsLow { get; set; } = 0;
+        public int OutStock { get; set; } = 0;
+        public DateTime CreateDate { get; set; } = DateTime.Now;
+    }
+}
---
+++ LineCat.Web/Models/UserProduct.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace LineCat.Web.Models
+{
+    public class UserProduct
+    {
+        public string ID { get; set; }
+        public string UserID { get; set; }
+        public string ProductID { get; set; }
+        public DateTime CreateDate { get; set; } = DateTime.Now;
+        [Display(Name = "排序")]
+        public int SortNum { get; set; } = 0;
+        [Display(Name = "停用")]
+        public int Disable { get; set; } = 0;
+
+        public virtual Product Product { get; set; }
+    }
+}
