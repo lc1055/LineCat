60058d0277f91089e3291b8d6c52049b2b27f83e
lc1055 <lc1055@163.com>
2015-12-08 18:14:19 +0800

    fix bug

--- LineCat.Web/Controllers/AdminController.cs
+++ LineCat.Web/Controllers/AdminController.cs
@@ -6,12 +6,27 @@ using System.Web.Mvc;
 
 namespace LineCat.Web.Controllers
 {
-    public class AdminController : Controller
+    public class AdminController : DBController
     {
         // GET: Admin
         public ActionResult Index()
         {
             return View();
+        }
+
+        public ActionResult Delete()
+        {
+            try
+            {
+                DateTime d = DateTime.Now.AddDays(-1);
+                db.Database.ExecuteSqlCommand("delete from pricehistory where islow=0 and createdate < " + d.ToString("yyyy/MM/dd"));
+                return Json(new { success = true });
+            }
+            catch (Exception e)
+            {
+                Common.TxtLog.SaveErrorLog(e.ToString());
+                return Json(new { success = false, msg = e.Message });
+            }
         }
     }
 }
