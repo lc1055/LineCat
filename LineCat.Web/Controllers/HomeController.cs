a15bd87e90e548039d93d91738c0f682e98bab42
lc <lc1055@hotmail.com>
2015-12-25 23:02:32 +0800

    key query

--- LineCat.Web/Controllers/HomeController.cs
+++ LineCat.Web/Controllers/HomeController.cs
@@ -6,16 +6,16 @@ namespace LineCat.Web.Controllers
 {
     public class HomeController : DBController
     {
-        public ActionResult Index(string id, int? page, int? rows)
+        public ActionResult Index(string id, string key, int? page, int? rows)
         {
             //起始页
             int pageIndex = page ?? 1;
             //每页显示的条数
             int pageSize = rows ?? GlobalPageSize;
 
-            var list = db.PriceHistory.Where(m => string.IsNullOrEmpty(id) || m.ProductID == id)
-                //.OrderByDescending(m => m.IsLow)
-                .OrderByDescending(m => m.CreateDate)
+            var list = db.PriceHistory.Where(m => (string.IsNullOrEmpty(id) || m.ProductID == id)
+                && (string.IsNullOrEmpty(key) || m.Title.Contains(key))
+                ).OrderByDescending(m => m.CreateDate)
                 .Skip((pageIndex - 1) * pageSize).Take(pageSize).ToList();
             
             
